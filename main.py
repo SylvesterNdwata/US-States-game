e98eaa500c59ecb96d69c54e2345ef1f3b567767
sylvester ndwata <ndwatasylvester88gmail.com>
2025-11-18 17:09:33 +0100

    added function and fixed some errors

--- main.py
+++ main.py
@@ -18,21 +18,25 @@ marker = Turtle()
 marker.ht()
 
 data = pandas.read_csv("50_states.csv")
-print(data[data.state == "Ohio"].x.item())
-print(data[data.state == "Ohio"].y.item())
 
-while True:
+def marking_states(guessed_state):
+    x_cor = data[data.state == f"{guessed_state}"].x.item()
+    y_cor = data[data.state == f"{guessed_state}"].y.item()
+    marker.color("Black")
+    marker.penup()
+    marker.goto(x_cor, y_cor)
+    marker.write(arg=f"{guessed_state}", align="Center")
+    
+
+correct = True
+while correct:
     guessed_state = screen.textinput(title="Guess the state", prompt="What's another state name?").title()
-    if guessed_state in states_list:
+    while guessed_state in states_list:
         guessed_state = screen.textinput(title="Already Guessed", prompt="You already guessed that state! Try again.").title()
-    elif guessed_state in data["state"].values and guessed_state not in states_list:
+        marking_states(guessed_state)
+    if guessed_state in data["state"].values and guessed_state not in states_list:
         states_list.append(guessed_state)
-        x_cor = data[data.state == f"{guessed_state}"].x.item()
-        y_cor = data[data.state == f"{guessed_state}"].y.item()
-        marker.color("Black")
-        marker.penup()
-        marker.goto(x_cor, y_cor)
-        marker.write(arg=f"{guessed_state}", align="Center")
+        marking_states(guessed_state)
         states += 1
         screen.title(f"{states}/50 States Correct")
         if states == 50:
@@ -41,6 +45,7 @@ while True:
         screen.update()
     else:
         guessed_state = screen.textinput(title="Wrong State", prompt="Try again!").title()
+        marking_states(guessed_state)
 
 
 
